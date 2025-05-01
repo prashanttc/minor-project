1d839e699457340821482043a109cc0e55e54451
prashant <prashantchouhan711@gmail.com>
2025-05-02 00:16:09 +0530

    urfb

--- ai-chatbot/app/chatbot/model_utils.py
+++ ai-chatbot/app/chatbot/model_utils.py
@@ -5,7 +5,7 @@ import os
 def load_model():
     model_path = hf_hub_download(
         repo_id="TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
-        filename="TinyLlama-1.1B-Chat-v1.0.Q4_K_M.gguf",
+        filename="tinyllama-1.1b-chat-v1.0.Q2_K.gguf",
         cache_dir="./models"
     )
 
